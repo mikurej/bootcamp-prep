36c9a62bba6931805c3effc73d579106bf7ffa5d
mikurej <mikurej95@gmail.com>
2018-05-13 20:22:46 -0400

    Update fullstack-hq.js
    missing code
    copy/pasted code from codepen.io

--- workshops/04-scope/01-fullstack-hq/fullstack-hq.js
+++ workshops/04-scope/01-fullstack-hq/fullstack-hq.js
@@ -1,1 +1,18 @@
-// YOUR CODE BELOW
+let place = 'Planet Earth';
+
+function fullstackHQ(){
+  return middleFunction('United States');
+
+  function middleFunction(place) {
+    return innerFunction('New York State');
+
+    function innerFunction(place) {
+      let place = 'New York City';
+      return innermostFunction();
+
+      function innermostFunction() {
+        return 'Fullstack HQ is at ' + place + ', ' + place + ', ' + place + ', ' + place;
+      }
+    }
+  }
+}
